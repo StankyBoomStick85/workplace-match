3ec43c854e0430b024917e165837260ad04198d5
Joel DeToy <jdetoy85@gmail.com>
2026-05-04 22:10:15 -0500

    Fix SSR window error on account settings (Leaflet + localStorage safe)

--- app/account/settings/page.tsx
+++ app/account/settings/page.tsx
@@ -1,4 +1,11 @@
-import { AccountSettings } from "@/components/AccountSettings";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const AccountSettings = dynamic(
+  () => import("@/components/AccountSettings").then((mod) => mod.AccountSettings),
+  { ssr: false }
+);
 
 export default function AccountSettingsPage() {
   return <AccountSettings />;
